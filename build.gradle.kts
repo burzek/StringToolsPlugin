bd86e545d48823cf5927521dd7f74c68ff744d25
Boris Brinza <brinza@essential-data.sk>
2024-01-25 14:36:06 +0100

    plugin.xml update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,21 +5,18 @@ plugins {
 }
 
 group = "sk.araed.intellij.plugins.stringtools"
-version = "1.0-SNAPSHOT"
+version = "4.1"
 
 repositories {
   mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
   version.set("2023.1.5")
   plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
-  // Set the JVM compatibility versions
   withType<JavaCompile> {
     sourceCompatibility = "17"
     targetCompatibility = "17"
@@ -29,17 +26,8 @@ tasks {
   }
 
   patchPluginXml {
-    sinceBuild.set("231")
+    sinceBuild.set("145")
     untilBuild.set("241.*")
   }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
-
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
 }
